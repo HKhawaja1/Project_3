9c7c46179a528f91c86b7c74214e05902c45d690
Hamzah Khawaja <hamzahkhawaja1@gmail.com>
2024-09-18 20:43:40 +0100

    Basic HTML and Flask templates created for all 5 of my pages.

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+# Home Page Route (index.html)
 @app.route("/")
 def home():
-    return "My Flask Application"
+    return render_template('index.html')
+
+# About Page Route (about.html)
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+# Blog Page Route (blog.html)
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+# Contact Page Route (contact.html)
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+# Shop Page Route (shop.html)
+@app.route("/shop")
+def shop():
+    return render_template('shop.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
